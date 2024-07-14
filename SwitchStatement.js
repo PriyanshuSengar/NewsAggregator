4341f5e86371daedb1dbfae1a0a32c7ec9c35077
priyanshu <priyanshusengarps@gmail.com>
2024-07-14 17:43:02 +0530

    Switch Statement

---
+++ SwitchStatement.js
@@ -0,0 +1,16 @@
+const day='monday';
+// In switch case break is mandatory
+switch(day){
+    case 'sunday':
+        console.log("Holiday!");
+        break;
+        case 'monday':
+            console.log("working day");
+            break;
+        case 'tuesday':
+            console.log("another working day");
+            break;
+        case 'wednesday':
+            console.log("also a working day");
+            break;
+}
