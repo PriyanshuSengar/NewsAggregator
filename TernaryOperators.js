f0891506fb6cb627c8fc25a2aade17c2d2427142
priyanshu <priyanshusengarps@gmail.com>
2024-07-14 18:17:13 +0530

    Ternary Operators

---
+++ TernaryOperators.js
@@ -0,0 +1,4 @@
+// it is same as the if else statement 
+const age=23;
+age >=18?console.log("I like to drink mango shake"):console.log("I like to drink milkshake");
+console.log(`I like to drink ${age >= 18 ? 'ColdDrink':'MilkShake'}`);
