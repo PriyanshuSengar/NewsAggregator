3f015324648a1424e04f8aae2126de9de98a6233
priyanshu <priyanshusengarps@gmail.com>
2024-07-14 14:00:20 +0530

    TypeConversion

---
+++ TypeConversionAndCorsion.js
@@ -0,0 +1,14 @@
+const inputyear=2003;
+console.log(Number(inputyear));
+console.log(inputyear+20);
+console.log(Number('Luckey'));
+console.log(typeof NaN);
+console.log(String(23) , 23);
+// type corsion
+console.log('I am '+ 23 +' year old');
+console.log('23'+'10'+3);
+console.log('23'/'2');
+let n='1'+1;
+n=n-1;
+console.log(n);
+console.log('10'-'5'-'2'+'1');
