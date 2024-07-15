27d4015dc0ea2f462e5af1f400f8fcf47d9fea05
priyanshu <priyanshusengarps@gmail.com>
2024-07-15 11:26:34 +0530

    Functions

---
+++ Functions.js
@@ -0,0 +1,13 @@
+// Function is a piece of code which we can use agian and again in our code
+function logger(){
+    console.log('My name is Luckey');
+}
+// calling,invoking,running function
+logger();
+function  fruitprocessor(apples,oranges){
+console.log(apples,oranges);
+const juice =`Juice with ${apples} apples and ${oranges} oranges .`;
+return juice; 
+}
+fruitprocessor(5,0);
+console.log(fruitprocessor(5,8));
