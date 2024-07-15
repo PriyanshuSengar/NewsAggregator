f0625108e62bc0425523cd5637344ea243b060d7
priyanshu <priyanshusengarps@gmail.com>
2024-07-15 10:57:33 +0530

    Strict Mode

---
+++ StrictMode.js
@@ -0,0 +1,6 @@
+'use strict'; // to check make the strict mode as comment
+let hasDriversLicense=false;
+const passTest=true;
+if(passTest) hasDriverLicense =true;
+if(hasDriverLicense)console.log("You are eligible for Driving");
+
