91b5769c9e90b43ff11b8aea2eafc172a8f805eb
priyanshu <priyanshusengarps@gmail.com>
2024-08-08 21:54:45 +0530

    Introduction To Objects

---
+++ IntroductionToObjects.js
@@ -0,0 +1,8 @@
+const luckeyArray = ["Luckey", "Sengar", 2024 - 2003, ["Luckey", "Sengar"]];
+const luckey = {
+  firstName: "Luckey",
+  lastName: "Sengar",
+  age: 2024 - 2003,
+  friends: ["Luckey", "Sengar"],
+};
+    
