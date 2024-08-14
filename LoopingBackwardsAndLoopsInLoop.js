f99c1a1f34f90b964a55151bd98923a3b71b4107
priyanshu <priyanshusengarps@gmail.com>
2024-08-14 21:37:31 +0530

    Looping Backwards and Loops in Loop

---
+++ LoopingBackwardsAndLoopsInLoop.js
@@ -0,0 +1,17 @@
+const luckey=[
+'jonas',
+'sengar',
+2003-2024,
+'teacher',
+['luckey','sengar','priyanshu']
+];
+for(let i=luckey.length-1; i>=0 ; i--){
+console.log(i,luckey[i]);
+}
+for(let exercise=1;exercise<4;exercise){
+    console.log(`-------- Starting ${exercise}`);
+    for(let rep=1;rep<6;rep++){
+        console.log(`lifting Weight repetation ${rep} `);
+
+    }
+}
